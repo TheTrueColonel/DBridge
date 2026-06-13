02abf6b6fd32061afbc82798662ca6c65e6c67d8
Benjamin <benjaminesims@gmail.com>
2026-06-13 16:23:48 -0600

    1.0.2

--- src/main/java/com/thetruecolonel/dbridge/DBridge.java
+++ src/main/java/com/thetruecolonel/dbridge/DBridge.java
@@ -23,10 +23,10 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-@Mod(modid = DBridge.MODID, version = DBridge.VERSION, name = DBridge.NAME, acceptableRemoteVersions = "[1.7.10]")
+@Mod(modid = DBridge.MODID, version = DBridge.VERSION, name = DBridge.NAME, acceptableRemoteVersions = "*")
 public class DBridge {
     public static final String MODID = "ttcdbridge";
-    public static final String VERSION = "1.0.1";
+    public static final String VERSION = "1.0.2";
     public static final String NAME = "Discord Bridge";
     public static final Logger LOG = LogManager.getLogger(MODID);
 
